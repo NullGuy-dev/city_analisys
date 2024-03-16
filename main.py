5fb14f99822c6e024d7018b8abaa23d0d7735b3b
null <86558426+NullGuy-dev@users.noreply.github.com>
2024-03-16 18:53:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+import keras
 from keras.models import Sequential, load_model
 from keras.layers import Dense
 
@@ -16,19 +17,23 @@ Y = dataset[:, 0]
 scaler = preprocessing.MinMaxScaler()
 X_scale = scaler.fit_transform(X)
 
-X_train, X_test, Y_train, Y_test = train_test_split(X_scale, Y, test_size=0.3)
+X_train, X_test, Y_train, Y_test = train_test_split(X_scale, Y, test_size=0.2)
 
 # creating a model
 model = Sequential([
-    Dense(32, activation='relu', input_shape=(4,)),
-    Dense(32, activation='relu'),
+    Dense(16, activation='relu', input_shape=(4,)),
     Dense(1, activation='sigmoid'),
 ])
 
-model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy']) # model's settings
-model.fit(X_train, Y_train, batch_size=16, epochs=800) # training model
-model.save('CA_model.h5') # saving model
+model.compile(optimizer=keras.optimizers.RMSprop(0.01), loss='binary_crossentropy', metrics=['accuracy']) # model's settings
+model.fit(X_train, Y_train, batch_size=8, epochs=30) # training model
+keras.saving.save_model(model, "city_analisys_model.keras") # saving model
 
 # And, testing our model
 print("\nTEST\n")
 model.evaluate(X_test, Y_test)
+
+# model = load_model("city_analisys_model.keras")
+
+for res in model.predict(X_test):
+    print(res[0])
